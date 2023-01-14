5f7eb7628546344ada56f8b1f1ddf104ff011e13
ZeroMinus13 <zerominus13@gmail.com>
2023-01-14 08:50:47 +0500

    added script for railway

--- app.js
+++ app.js
@@ -75,4 +75,4 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-app.listen(process.env.PORT || 3000), () => console.log('app listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () => console.log('app listening on port 3000!'));
